c83e02405fea589bccead09e62b2665dfaf17b34
Arezalgamer89 <86628069+ArezalGame89@users.noreply.github.com>
2021-12-22 18:54:22 +0330

    Scrap Aqua (A private lua framework)

--- Lua/scipt.lua
+++ Lua/scipt.lua
@@ -1,2 +1,12 @@
 io.write("Hello world, from ",_VERSION,"!\n")
 --This command prints a HELLO WORLD, with also saying what lua version you're running it from, if you have it that is... https://www.lua.org/download.html
+
+
+-- GOD PLEASE DON'T LOOK AT THIS SHITTY SCRAP
+
+--function onCreate()
+--io.system("set", "varCMND")
+--setProperty("varCMND", "", "PrintFunc")
+--setDebugPrint("" + PrintFunc)
+--setPrint("HelloWorld", "ID" + "hw")
+--print("" + ToString.ID.hw)
